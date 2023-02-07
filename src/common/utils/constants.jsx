3a4f4e723abf0265422ff50fe1c0a45a2ab83f8d
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-02-07 22:56:20 +0200

    add constants

---
+++ src/common/utils/constants.jsx
@@ -0,0 +1,8 @@
+export const Colors = {
+  softOrange: "#e9ab5",
+  softRed: "#f15e50",
+  offWhite: "#fffdfa",
+  grayishBlue: "#c5c6ce",
+  darkGrayishBlue: "#5d5f79",
+  veryDarkBlue: "#00001a",
+};
